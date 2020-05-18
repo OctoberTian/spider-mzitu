aed1ad246fd6344b4e0c60701425aaca38c75df0
seepine <seepine@163.com>
2020-05-18 17:44:02 +0800

    add annotation

--- src/main/java/com/seepine/mzitu/constant/CommonConstant.java
+++ src/main/java/com/seepine/mzitu/constant/CommonConstant.java
@@ -5,10 +5,24 @@ package com.seepine.mzitu.constant;
  * @date 2020-05-17 19:35
  */
 public interface CommonConstant {
+    /**
+     * 等待爬取信息缓存命名空间
+     */
     String AWAIT_CACHE_NAME = "await";
+    /**
+     * 已爬取url缓存命名空间
+     */
     String ALREADY_CACHE_NAME = "already";
-
+    /**
+     * 图片下载延迟毫秒
+     */
     long DOWNLOAD_MILLIS = 1000;
+    /**
+     * 页面爬取延迟毫秒
+     */
     int CRAWL_MILLIS = 5000;
+    /**
+     * 页面爬取线程数
+     */
     int CRAWL_THREAD_NUM = 1;
 }
